b190d71de0098eac3c4263efbd1099cc36eed446
T-Skipper <wangwetymothy@gmail.com>
2020-05-06 01:07:10 +0300

    Manual run

--- Covid-19/Covid-19 (Simple Pie Charts).py
+++ Covid-19/Covid-19 (Simple Pie Charts).py
@@ -13,43 +13,49 @@ try:
     data = pd.read_csv(path, sep=',', index_col=0)
     data.head()
 except FileNotFoundError:
-    raise SystemExit(f'{path}\nFile in the above path does not exist.') # Stop execution at this point
+    # Stop execution at this point
+    raise SystemExit(f'{path}\nFile in the above path does not exist.')
 # Package pie plot as a function to call it later on
+
+
 def visualize(country, data):
     labels = [f'Total Deaths: {TotalDeaths}',
               f'Total Recoveries: {TotalRecoveries}',
               f'Active Cases: {ActiveCases}']
     colors = ['crimson', 'forestgreen', 'tab:blue']
-    
-    df.plot(kind = 'pie',
-            subplots = True,
-            figsize = (7, 7),
-            autopct = "%0.0f%%",
-            shadow = True,
-            explode = (0,0.05,0.05),
-            labels = labels,
-            colors = colors
-           )
-    plt.suptitle(f'{country}\n', 
-                 size = 18,
-                 weight = "bold",
-                 color = "tab:green",
-                 y = 1
-                )
-    plt.title(f'Covid-19 Summary\n{file} @ {TotalCases} Cases', color="teal", fontsize="12")
-    plt.legend(title = f'Total Cases: {TotalCases}',
-               loc = 'best', 
-               bbox_to_anchor = (1, 0.75),
-               markerfirst = False,
-               framealpha = 0.5
-              )
+
+    df.plot(kind='pie',
+            subplots=True,
+            figsize=(7, 7),
+            autopct="%0.0f%%",
+            shadow=True,
+            explode=(0, 0.05, 0.05),
+            labels=labels,
+            colors=colors
+            )
+    plt.suptitle(f'{country}\n',
+                 size=18,
+                 weight="bold",
+                 color="tab:green",
+                 y=1
+                 )
+    plt.title(
+        f'Covid-19 Summary\n{file} @ {TotalCases} Cases', color="teal", fontsize="12")
+    plt.legend(title=f'Total Cases: {TotalCases}',
+               loc='best',
+               bbox_to_anchor=(1, 0.75),
+               markerfirst=False,
+               framealpha=0.5
+               )
     plt.axis("off")
     plt.savefig(f'Images/{Name}({file})',
-                format = 'svg', 
-                quality = 95, 
-                transparent = True, 
-                bbox_inches = 'tight'
-               )
+                format='svg',
+                quality=95,
+                transparent=True,
+                bbox_inches='tight'
+                )
+
+
     # plt.show()
 # Prepare data to fit in the function created above
 for count in range(data['Countries'].count()):
@@ -59,11 +65,12 @@ for count in range(data['Countries'].count()):
     TotalDeaths = country[3]
     TotalRecoveries = country[5]
     ActiveCases = TotalCases - (TotalDeaths + TotalRecoveries)
-    
-    df = pd.DataFrame([TotalDeaths,TotalRecoveries, ActiveCases],
-    index = ['Total Deaths', 'Total Recoveries', 'Active Cases'])
-    
-    if df[0][0] != 0 or df[0][1] != 0 or df[0][2] != 0: # To avoid errors raised when values are null
+
+    df = pd.DataFrame([TotalDeaths, TotalRecoveries, ActiveCases],
+                      index=['Total Deaths', 'Total Recoveries', 'Active Cases'])
+
+    # To avoid errors raised when values are null
+    if df[0][0] != 0 or df[0][1] != 0 or df[0][2] != 0:
         visualize(Name, df)
     else:
         print(f'''
--- Covid-19/Covid-19-Datasets/Covid-19-Api/Datasets/Readme.md
+++ Covid-19/Covid-19-Datasets/Covid-19-Api/Datasets/Readme.md
@@ -1,1 +1,1 @@
-File automatically updated at 11:14:20 on 29th of April 2020.
+File automatically updated at 00:50:35 on 06th of May 2020.
